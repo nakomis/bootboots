52e3703907dfe7e41e6e1def2bae3443c4752a67
Martin Harris <1488244+nakomis@users.noreply.github.com>
2025-07-15 19:48:38 +0000

    Adds syncaws.sh script

--- mq/imageclient/syncaws.sh
+++ mq/imageclient/syncaws.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
--e
+set -e
 
 export AWS_PROFILE=nakom.is-sandbox
+
+pushd receivedimages
+aws s3 sync . s3://bootbootstraining
+popd
+
 aws dynamodb scan --table-name catadata > catadata.json
 
 for f in $(ls -b receivedimages/*.jpeg | xargs basename); do
-    grep "$f" catadata.json > /dev/null || aws dynamodb put-item --table-name catadata --item "{\"imageName\":{\"S\":\""$f"\"}, \"uuid\":{\"S\": \"$(uuidgen)\"}}"i;
+    grep "$f" catadata.json > /dev/null || aws dynamodb put-item --table-name catadata --item "{\"imageName\":{\"S\":\""$f"\"}, \"uuid\":{\"S\": \"$(uuidgen)\"}}"
 done;
