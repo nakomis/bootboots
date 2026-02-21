4e0e919de9545bbee431f3db5dd1e2319c9d195a
Martin Harris <1488244+nakomis@users.noreply.github.com>
2026-02-21 17:40:57 +0000

    Exclude svg and fzz files from cloc

--- scripts/tloc.sh
+++ scripts/tloc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # cloc . --by-file --exclude-dir=.pio,ignored,.vscode,catcam-images,node_modules,cdk.out,.claude,dist --not-match-f="package-lock"
-cloc . --exclude-dir=.pio,ignored,.vscode,catcam-images,node_modules,cdk.out,.claude,dist --not-match-f="package-lock"
+cloc . --exclude-dir=.pio,ignored,.vscode,catcam-images,node_modules,cdk.out,.claude,dist --not-match-f="package-lock" --not-match-f=".svg" --not-match-f=".fz"
